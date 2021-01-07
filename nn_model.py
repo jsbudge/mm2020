925654fae71669d7133536b8088e7622d6f8ed1f
Jeff Budge <jbudge@artemisinc.net>
2021-01-07 16:22:27 -0700

    Added hyperparameter tuning to the Tensorboard. Doing a few runs to see what works and what doesn't.

--- nn_model.py
+++ nn_model.py
@@ -36,6 +36,7 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras import layers
 from keras.callbacks import TensorBoard
 from tensorflow.keras import backend as K
+from tensorboard.plugins.hparams import api as hp
 from datetime import datetime
 plt.close('all')
 
@@ -45,39 +46,38 @@ state_sz: the size of each feature vector to be used in training/prediction.
 optimizer: the optimization method for the model. Adam is the default,
             but there are plenty of others.
 '''
-def compile_model(state_sz, optimizer='adam'):
+def compile_model(state_sz, layer_sz, n_layers, optimizer='adam', metrics=['accuracy']):
+    dropout_rate = .2
     inp1 = keras.Input(shape=(state_sz,))
     inp2 = keras.Input(shape=(state_sz,))
     mdlconc = layers.Subtract()([inp1, inp2])
-    out = layers.Dense(60, activation='relu')(mdlconc)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dropout(.3)(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.BatchNormalization()(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dropout(.3)(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dropout(.3)(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.BatchNormalization()(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dense(50, activation='relu')(out)
-    out = layers.Dropout(.3)(out)
-    out = layers.Dense(50, activation='relu')(out)
+    out = layers.Dense(layer_sz, activation='relu', name='init_dense')(mdlconc)
+    for n in range(n_layers):
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_0'.format(n))(out)
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_1'.format(n))(out)
+        out = layers.Dropout(dropout_rate)(out)
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_2'.format(n))(out)
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_3'.format(n))(out)
+        out = layers.BatchNormalization(name='bn_{}'.format(n))(out)
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_4'.format(n))(out)
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_5'.format(n))(out)
+        out = layers.Dense(layer_sz, activation='relu',
+                           name='dense_{}_6'.format(n))(out)
+        out = layers.Dropout(dropout_rate)(out)
+    out = layers.Dense(layer_sz, activation='relu',
+                       name='final_dense')(out)
     smax = layers.Dense(2, activation='softmax',
                         name='output')(out)
     model = keras.Model(inputs=[inp1, inp2], outputs=smax)
     model.compile(optimizer=optimizer,
                   loss=['binary_crossentropy'],
-                  metrics=['accuracy'])
+                  metrics=metrics)
     return model
 
 '''
@@ -99,6 +99,7 @@ files = st.getFiles()
 #%%
 
 nspl = 5
+tune_hyperparams = False
 scale = StandardScaler()
 cv = KFold(n_splits=nspl, shuffle=True)
 scale_df = fl.arrangeFrame(files, scaling=StandardScaler(), noinfluence=True)
@@ -130,43 +131,88 @@ prob_df = sdf.drop(columns=[c for c in sdf.columns if c[:1] == 'O'] + \
 pdf = prob_df.groupby(['GameID']).first() - prob_df.groupby(['GameID']).last()
 pdf = pdf.append(-pdf).sort_index().set_index(prob_df.sort_index().index)
 
-kpca = decomp.TruncatedSVD(n_components=25)
+kpca = decomp.TruncatedSVD(10)
 kpca.fit(pdf)
 ff = pd.DataFrame(index=pdf.index, data=kpca.transform(pdf)).groupby(['Season', 'TID']).mean()
-# plt.figure('KPCA Eigenvalues')
-# plt.subplot(211)
-# plt.plot(kpca.lambdas_)
-# plt.subplot(212)
-# sns.heatmap(ff.cov())
 
-g1, g2, outcomes = get_frames(sdf.index, ff, scale_df[1])
-e1, e2, e_out = get_frames(games[0].index, ff, games[1])
+cmb_df = avdf.merge(ff, left_index=True, right_index=True)
+
+g1, g2, outcomes = get_frames(sdf.index, cmb_df, scale_df[1])
+e1, e2, e_out = get_frames(games[0].index, cmb_df, games[1])
 
 #%%
 K.clear_session()
 
-learn_rate = 1e-4
-num_epochs = 5000
-callbacks = [tf.keras.callbacks.EarlyStopping(
-                    monitor="loss",
+HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([20, 40, 60, 100]))
+HP_NUM_LAYERS = hp.HParam('num_layers', hp.Discrete([1, 2, 3, 4]))
+HP_LR = hp.HParam('learning_rate', hp.Discrete([1e-2, 1e-3, 1e-4]))
+
+METRIC_ACCURACY = 'accuracy'
+
+with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
+    hp.hparams_config(
+      hparams=[HP_NUM_UNITS, HP_NUM_LAYERS, HP_LR],
+      metrics=[hp.Metric(METRIC_ACCURACY, display_name='Accuracy')],
+    )
+
+learn_rate = 1e-3
+num_epochs = 300
+logdir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+k_calls = [tf.keras.callbacks.EarlyStopping(
+                    monitor="val_loss",
                     min_delta=1e-4,
-                    patience=200,
+                    patience=50,
                     verbose=0,
                     mode="auto",
                     baseline=None,
                     restore_best_weights=True),
             TensorBoard(histogram_freq=3, write_images=False,
-                        log_dir="logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S"))]
+                        log_dir=logdir)]
+metrics = ['accuracy',
+           tf.keras.metrics.AUC(),
+           tf.keras.metrics.Precision(),
+           tf.keras.metrics.Recall()]
 
 opt_adam = keras.optimizers.Adam(learning_rate=learn_rate)
-model = compile_model(g1.shape[1], optimizer=opt_adam)
+
 
 t_pts, v_pts = next(cv.split(outcomes))
 Xt1, Xs1 = g1.iloc[t_pts], g1.iloc[v_pts]
 Xt2, Xs2 = g2.iloc[t_pts], g2.iloc[v_pts]
 yt, ys = outcomes[t_pts], outcomes[v_pts]
-hist = model.fit([Xt1, Xt2], yt, epochs=num_epochs, validation_data=([Xs1, Xs2], ys),
-          callbacks=callbacks, verbose=2, batch_size=400)
+
+if tune_hyperparams:
+    session_num = 0
+    for num_units in HP_NUM_UNITS.domain.values:
+        for num_layers in HP_NUM_LAYERS.domain.values:
+            for lrate in HP_LR.domain.values:
+                hparams = {
+                    HP_NUM_UNITS: num_units,
+                    HP_NUM_LAYERS: num_layers,
+                    HP_LR: lrate,
+                }
+                callbacks = k_calls + [hp.KerasCallback(logdir, hparams)]
+                model = compile_model(g1.shape[1], hparams[HP_NUM_UNITS], 
+                                      hparams[HP_NUM_LAYERS], 
+                                      optimizer = keras.optimizers.Adam(learning_rate=hparams[HP_LR]),
+                                      metrics=metrics)
+                run_name = "run-%d" % session_num
+                print('--- Starting trial: %s' % run_name)
+                print({h.name: hparams[h] for h in hparams})
+                with tf.summary.create_file_writer('logs/hparam_tuning/' + run_name).as_default():
+                    hp.hparams(hparams)  # record the values used in this trial
+                    model.fit([Xt1, Xt2], yt, epochs=num_epochs, validation_data=([Xs1, Xs2], ys),
+                              callbacks=callbacks, verbose=2, batch_size=200)
+                    mets = model.evaluate([Xs1, Xs2], ys)
+                    tf.summary.scalar(METRIC_ACCURACY, mets[1], step=1)
+                session_num += 1
+else:
+    model = compile_model(g1.shape[1], 80, 
+                        1, 
+                        optimizer = keras.optimizers.Adam(learning_rate=1e-4),
+                        metrics=metrics)
+    model.fit([Xt1, Xt2], yt, epochs=num_epochs, validation_data=([Xs1, Xs2], ys),
+              callbacks=k_calls, verbose=2, batch_size=200)
 model.evaluate([e1, e2], e_out)
 
 

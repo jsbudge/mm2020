00e62bffdf8f1078f419451a076b7ce150aa9c26
jbudge <jbudge@artemisinc.net>
2021-02-11 16:03:57 -0700

    Added some ML-based stat reduction strategies to try and distill things down to what we really need.

--- stats_analysis.py
+++ stats_analysis.py
@@ -19,12 +19,13 @@ import eventlib as ev
 from scipy.stats import multivariate_normal as mvn
 from scipy.stats import iqr, chi2_contingency
 from scipy.interpolate import CubicSpline
-from sklearn.metrics import mutual_info_score
+from sklearn.metrics import mutual_info_score, log_loss
 from sklearn.cluster import FeatureAgglomeration
 from sklearn.decomposition import KernelPCA
-from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.feature_selection import RFE
-from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.svm import SVC
 from itertools import combinations
 from scipy.optimize import minimize
 plt.close('all')
@@ -56,6 +57,29 @@ def getAllAverages(tid):
         ret = ret.append(t)
     return ret.set_index(['Season', 'TID', 'AvType'])
 
+def integerOpt(func, rng, npts=5, nits=5):
+    curr_rng = np.linspace(rng[0], rng[1], npts).astype(int)
+    curr_vals = np.array([func(i) for i in curr_rng])
+    curr_min = np.min(curr_vals)
+    m_idx = [n for n in range(npts) if curr_vals[n] == curr_min][0]
+    global_min = curr_min + 0.0; global_val = curr_rng[m_idx]
+    for it in tqdm(range(nits)):
+        if m_idx == 0:
+            curr_rng = np.linspace(curr_rng[0], curr_rng[1], npts).astype(int)
+        elif m_idx == npts-1:
+            curr_rng = np.linspace(curr_rng[-2], curr_rng[-1], npts).astype(int)
+        else:
+            curr_rng = np.linspace(curr_rng[m_idx-1], curr_rng[m_idx+1], npts).astype(int)
+        curr_vals = np.array([func(i) for i in curr_rng])
+        curr_min = np.min(curr_vals)
+        m_idx = [n for n in range(npts) if curr_vals[n] == curr_min][0]
+        if curr_min < global_min:
+            global_min = curr_min
+            global_val = curr_rng[m_idx]
+    return global_val
+        
+        
+
 sdf, sdf_t, sdf_d = st.arrangeFrame(scaling=None, noinfluence=True)
 avs = {}
 m_types = ['relelo', 'gausselo', 'elo', 'rank', 'mest', 'mean', 'recent']
@@ -69,16 +93,16 @@ print('Scaling for influence...')
 inf_df = st.getInfluenceStats(sdf).set_index(['Season', 'TID'])
 
 #%%
-t_inf = st.getMatches(sdf, inf_df, diff=True)
-t_ps = st.getMatches(sdf, tsdf, diff=True)
+t_inf = st.getMatches(tdf, inf_df, diff=True)
+t_ps = st.getMatches(tdf, tsdf, diff=True)
 t_adv = st.getMatches(tdf, adv_tdf.drop(columns=['T_RoundRank']), diff=True)
-av_tdf = st.getMatches(sdf, avs['recent'], diff=True)
-tdf_diff = st.merge(av_tdf, t_inf, t_ps)
+av_tdf = st.getMatches(tdf, avs['recent'], diff=True)
+tdf_diff = st.merge(av_tdf, t_inf, t_ps, t_adv)
 
 #%%
 ntdiff = tdf_diff.dropna()
-ntdiff = ntdiff.loc[ntdiff.index.get_level_values(0).duplicated(keep='last')]
-scores = sdf.loc[ntdiff.index, 'T_Score'] - sdf.loc[ntdiff.index, 'O_Score']
+#ntdiff = ntdiff.loc[ntdiff.index.get_level_values(0).duplicated(keep='last')]
+scores = tdf.loc[ntdiff.index, 'T_Score'] - tdf.loc[ntdiff.index, 'O_Score']
 for s, grp in ntdiff.groupby(['Season']):
     ntdiff.loc[grp.index] = (grp - grp.mean()) / grp.std()
 
@@ -135,8 +159,27 @@ ys = scores.values[te_s]
 Xt = kdf.iloc[tr_s]
 Xs = kdf.iloc[te_s]
 
-svr = SVR(kernel='linear')
+svr = SVC(kernel='linear')
 rfe_select = RFE(svr)
-rfe_select.fit(Xt, yt)
+rfe_select.fit(Xt, yt > 0)
+sel_cols = kdf.columns[rfe_select.ranking_ == 1]
 
+#%%
+est_res = pd.DataFrame(columns=['n_tr', 'crit', 'acc', 'log_likelihood'])
+counter = 0
+for n_tr in tqdm(np.arange(10, 1000, 5)):
+    for criterion in ['gini', 'entropy']:
+        rfc = RandomForestClassifier(n_estimators=n_tr,
+                                     criterion=criterion).fit(Xt[sel_cols], yt > 0)
+        est_res.loc[counter] = [n_tr, criterion, rfc.score(Xs[sel_cols], ys > 0), 
+                                 log_loss(ys > 0, rfc.predict_proba(Xs[sel_cols])[:, 1])]
+        counter += 1
+        
+def min_func(n_tr):
+    rfc = RandomForestClassifier(n_estimators=n_tr).fit(Xt[sel_cols], yt > 0)
+    return 1 - rfc.score(Xs[sel_cols], ys > 0)
 
+ntree_opt = integerOpt(min_func, [10, 1000], 20)
+rfc = RandomForestClassifier(n_estimators=ntree_opt).fit(Xt[sel_cols], yt > 0)
+print('High score is {} trees with {:.2f}% acc.'.format(ntree_opt, 
+                                                        rfc.score(Xs[sel_cols], ys > 0) * 100))

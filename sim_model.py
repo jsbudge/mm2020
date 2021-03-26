04812ddbdc734abe3a5746390d8813dd0a94783b
Jeff <penryn101@gmail.com>
2021-03-25 22:10:17 -0600

    Added to simulation. It actually runs quite well now, and much faster in parallel. Also added some more submissions that are pretty bad.

--- sim_model.py
+++ sim_model.py
@@ -23,17 +23,12 @@ from sklearn.decomposition import KernelPCA, TruncatedSVD
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.preprocessing import StandardScaler, PowerTransformer, OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.covariance import oas
 import matplotlib.pyplot as plt
 from tourney import Bracket
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras import layers, regularizers
-from keras.callbacks import TensorBoard
-from tensorflow.keras import backend as K
 from datetime import datetime
+import multiprocessing as mp
 
 def shuffle(df):
     idx = np.random.permutation(np.arange(df.shape[0]))
@@ -56,7 +51,7 @@ scale.fit(df1)
 df1 = rescale(df1, scale)
 df2 = rescale(df2, scale)
 
-tsvd = TruncatedSVD(n_components=20)
+tsvd = TruncatedSVD(n_components=30)
 tsvd.fit(df2)
 
 df1 = pd.DataFrame(index=df1.index, data=tsvd.transform(df1))
@@ -73,22 +68,22 @@ Xt = rescale(Xt, scale)
 Xs = rescale(Xs, scale)
 
 #%%
-
-rfc = RandomForestClassifier(n_estimators=500)
+print('Fitting estimator...')
+rfc = AdaBoostClassifier(n_estimators=250, learning_rate=1e-3)
 rfc.fit(Xt, yt)
 
 #%%
 
-k_mu = df1.groupby(['Season', 'TID']).mean()
+k_mu = df1.groupby(['Season', 'TID']).apply(lambda x: np.average(x, axis=0, weights=np.arange(x.shape[0])**2))
 k_std = pd.DataFrame(index=k_mu.index, columns=['COV'])
 for idx, grp in df1.groupby(['Season', 'TID']):
     k_std.loc[idx, 'COV'] = oas(grp)[0]
 
 #%%
 print('Running simulations...')
-names = st.loadTeamNames()
-sim_res = pd.DataFrame(index=tdf.index, columns=['T%', 'TName', 'O%', 'OName'])
-for idx, row in tdf.iterrows():
+
+def sim_games(x):
+    idx, row = x
     t_mu = k_mu.loc[(idx[1], idx[2])]
     t_cov = k_std.loc[(idx[1], idx[2])][0]
     t_gen = np.random.multivariate_normal(t_mu, t_cov, 100)
@@ -96,10 +91,18 @@ for idx, row in tdf.iterrows():
     o_cov = k_std.loc[(idx[1], idx[3])][0]
     o_gen = np.random.multivariate_normal(o_mu, o_cov, 100)
     res = rfc.predict(t_gen - o_gen)
-    sim_res.loc[idx, ['T%', 'TName', 'O%', 'OName']] = [sum(res) / 100, names[idx[2]], 
-                                                        1 - sum(res) / 100, names[idx[3]]]
+    return [sum(res) / 100, names[idx[2]], 
+            1 - sum(res) / 100, names[idx[3]]]
     
+    
+names = st.loadTeamNames()
+proc = mp.Pool(5)
+data = [(idx, row) for idx, row in tdf.iterrows()]
+sim_res = pd.DataFrame(index=tdf.index,
+                       columns=['T%', 'TName', 'O%', 'OName'],
+                       data=proc.map(sim_games, [(idx, row) for idx, row in tdf.iterrows()]))
 #%%
+n_sims = 400
 subs = pd.read_csv('./data/MSampleSubmissionStage2.csv')
 sub_df = pd.DataFrame(columns=['GameID'], data=np.arange(subs.shape[0]),
                       dtype=int)
@@ -117,16 +120,20 @@ sim_res = pd.DataFrame(index=tdf.index, columns=['T%', 'TName', 'O%', 'OName'])
 for idx, row in sub_df.append(s2_df).iterrows():
     t_mu = k_mu.loc[(idx[1], idx[2])]
     t_cov = k_std.loc[(idx[1], idx[2])][0]
-    t_gen = np.random.multivariate_normal(t_mu, t_cov, 100)
+    t_gen = np.random.multivariate_normal(t_mu, t_cov, n_sims)
     o_mu = k_mu.loc[(idx[1], idx[3])]
     o_cov = k_std.loc[(idx[1], idx[3])][0]
-    o_gen = np.random.multivariate_normal(o_mu, o_cov, 100)
+    o_gen = np.random.multivariate_normal(o_mu, o_cov, n_sims)
     res = rfc.predict(t_gen - o_gen)
-    sim_res.loc[idx, ['T%', 'TName', 'O%', 'OName']] = [1 - sum(res) / 100, names[idx[2]], 
-                                                        sum(res) / 100, names[idx[3]]]
+    sim_res.loc[idx, ['T%', 'TName', 'O%', 'OName']] = [1 - sum(res) / n_sims, names[idx[2]], 
+                                                        sum(res) / n_sims, names[idx[3]]]
 
+sim_res = sim_res.dropna()
 
-
+#%%
+sim_check = sim_res.drop(columns=['TName', 'OName'])
+br2021 = Bracket(2021)
+br2021.runWithFrame(sim_check)
 
     
     
